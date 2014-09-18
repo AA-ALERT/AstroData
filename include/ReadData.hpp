f84a1198846f137387ab643c7bd9e73872efad02
Alessio Sclocco <alessio@sclocco.eu>
2014-09-18 15:58:25 +0200

    Refactoring of the read functions complete.

--- include/ReadData.hpp
+++ include/ReadData.hpp
@@ -17,23 +17,9 @@
 #include <string>
 #include <cmath>
 #include <H5Cpp.h>
-using std::ios;
-using std::string;
-using std::ifstream;
-using std::vector;
-using std::sqrt;
-using H5::H5File;
-using H5::Group;
-using H5::DataSet;
-using H5::FloatType;
-using H5::IntType;
 
 #include <Observation.hpp>
-#include <CLData.hpp>
 #include <utils.hpp>
-using isa::OpenCL::CLData;
-using isa::utils::toStringValue;
-using isa::utils::bigEndianToLittleEndian;
 
 
 #ifndef READ_DATA_HPP
@@ -41,29 +27,27 @@ using isa::utils::bigEndianToLittleEndian;
 
 namespace AstroData {
 
-template< typename T > void readSIGPROC(Observation< T > & observation, unsigned int bytestoSkip, string inputFilename, vector< CLData< T > * > & data, unsigned int firstSecond = 0);
-template< typename T > void readLOFAR(string headerFilename, string rawFilename, Observation< T > &observation, vector< CLData< T > * > & data, unsigned int nrSeconds = 0, unsigned int firstSecond = 0);
+template< typename T > void readSIGPROC(Observation & observation, const unsigned int bytestoSkip, std::string inputFilename, std::vector< std::vector< T > * > & data, unsigned int firstSecond = 0);
+template< typename T > void readLOFAR(std::string headerFilename, std::string rawFilename, Observation & observation, std::vector< std::vector< T > * > & data, unsigned int nrSeconds = 0, unsigned int firstSecond = 0);
 
 
 // Implementation
 
-template< typename T > void readSIGPROC(Observation< T > & observation, unsigned int bytesToSkip, string inputFilename, vector< CLData< T > * > & data, unsigned int firstSecond) {
-	ifstream inputFile;
+template< typename T > void readSIGPROC(Observation & observation, unsigned int bytesToSkip, std::string inputFilename, std::vector< std::vector< T > * > & data, unsigned int firstSecond) {
+	std::ifstream inputFile;
 	const unsigned int BUFFER_DIM = 4;
 	char * buffer = new char [BUFFER_DIM];
 
-	inputFile.open(inputFilename.c_str(), ios::binary);
+	inputFile.open(inputFilename.c_str(), std::ios::binary);
 	inputFile.sync_with_stdio(false);
-	inputFile.seekg(bytesToSkip, ios::beg);
+	inputFile.seekg(bytesToSkip, std::ios::beg);
 	for ( unsigned int second = 0; second < observation.getNrSeconds(); second++ ) {
-		data.at(second) = new CLData< T >("second" + toStringValue< unsigned int >(second), true);
-		(data.at(second))->setDeviceReadOnly();
-		(data.at(second))->allocateHostData(observation.getNrSamplesPerPaddedSecond() * observation.getNrChannels());
+		data.at(second) = new std::vector< T >(observation.getNrSamplesPerPaddedSecond() * observation.getNrChannels());
 
 		for ( unsigned int sample = 0; sample < observation.getNrSamplesPerSecond(); sample++ ) {
 			for ( unsigned int channel = observation.getNrChannels(); channel > 0; channel-- ) {
 				inputFile.read(buffer, BUFFER_DIM);
-				(data.at(second))->setHostDataItem((static_cast< long long unsigned int >(channel - 1) * observation.getNrSamplesPerPaddedSecond()) + sample, *(reinterpret_cast< T * >(buffer)));
+        data.at(second)[(static_cast< long long unsigned int >(channel - 1) * observation.getNrSamplesPerPaddedSecond()) + sample] = *(reinterpret_cast< T * >(buffer));
 			}
 		}
 	}
@@ -73,36 +57,36 @@ template< typename T > void readSIGPROC(Observation< T > & observation, unsigned
 }
 
 
-template< typename T > void readLOFAR(string headerFilename, string rawFilename, Observation< T > &observation, vector< CLData< T > * > & data, unsigned int nrSeconds, unsigned int firstSecond) {
+template< typename T > void readLOFAR(std::string headerFilename, std::string rawFilename, Observation & observation, std::vector< std::vector< T > * > & data, unsigned int nrSeconds, unsigned int firstSecond) {
+  unsigned int nrSubbands, nrChannels;
+  float minFreq, channelBandwidth;
 	// Read the HDF5 file with the metadata
-	H5File headerFile = H5File(headerFilename, H5F_ACC_RDONLY);
-	FloatType typeDouble = FloatType(H5::PredType::NATIVE_DOUBLE);
+	H5::H5File headerFile = H5::H5File(headerFilename, H5F_ACC_RDONLY);
+	H5::FloatType typeDouble = H5::FloatType(H5::PredType::NATIVE_DOUBLE);
 	double valueDouble = 0.0;
-	IntType typeUInt = IntType(H5::PredType::NATIVE_UINT);
+	H5::IntType typeUInt = H5::IntType(H5::PredType::NATIVE_UINT);
 	unsigned int valueUInt = 0;
 
-	Group currentNode = headerFile.openGroup("/");
-	currentNode.openAttribute("OBSERVATION_FREQUENCY_MAX").read(typeDouble, reinterpret_cast< void * >(&valueDouble));
-	observation.setMaxFreq(valueDouble);
+	H5::Group currentNode = headerFile.openH5::Group("/");
 	currentNode.openAttribute("OBSERVATION_FREQUENCY_MIN").read(typeDouble, reinterpret_cast< void * >(&valueDouble));
-	observation.setMinFreq(valueDouble);
-	currentNode = currentNode.openGroup(currentNode.getObjnameByIdx(0));
+  minFreq = valueDouble;
+	currentNode = currentNode.openH5::Group(currentNode.getObjnameByIdx(0));
 	currentNode.openAttribute("TOTAL_INTEGRATION_TIME").read(typeDouble, reinterpret_cast< void * >(&valueDouble));
 	double totalIntegrationTime = valueDouble;
 	currentNode.openAttribute("NOF_BEAMS").read(typeUInt, reinterpret_cast< void * >(&valueUInt));
 	observation.setNrBeams(valueUInt);
-	currentNode = currentNode.openGroup(currentNode.getObjnameByIdx(0));
+	currentNode = currentNode.openH5::Group(currentNode.getObjnameByIdx(0));
 	currentNode.openAttribute("NOF_SAMPLES").read(typeUInt, reinterpret_cast< void * >(&valueUInt));
 	unsigned int totalSamples = valueUInt;
 	currentNode.openAttribute("NOF_STATIONS").read(typeUInt, reinterpret_cast< void * >(&valueUInt));
 	observation.setNrStations(valueUInt);
 	currentNode.openAttribute("CHANNELS_PER_SUBBAND").read(typeUInt, reinterpret_cast< void * >(&valueUInt));
-	unsigned int nrChannels = valueUInt;
+	nrChannels = valueUInt;
 	currentNode.openAttribute("CHANNEL_WIDTH").read(typeDouble, reinterpret_cast< void * >(&valueDouble));
-	observation.setChannelBandwidth(valueDouble / 1000000);
-	DataSet currentData = currentNode.openDataSet("STOKES_0");
+  channelBandwidth = valueDouble / 1000000;
+	H5::DataSet currentData = currentNode.openH5::DataSet("STOKES_0");
 	currentData.openAttribute("NOF_SUBBANDS").read(typeUInt, reinterpret_cast< void * >(&valueUInt));
-	unsigned int nrSubbands = valueUInt;
+	nrSubbands = valueUInt;
 	headerFile.close();
 
 	observation.setNrSamplesPerSecond(static_cast< unsigned int >(totalSamples / totalIntegrationTime));
@@ -115,29 +99,26 @@ template< typename T > void readLOFAR(string headerFilename, string rawFilename,
 			observation.setNrSeconds(static_cast< unsigned int >(totalIntegrationTime) - firstSecond);
 		}
 	}
-	observation.setNrChannels(nrChannels * nrSubbands);
+  observation.setFrequencyRange(nrSubbands * nrChannels, minFreq, channelBandwidth);
 
 	// Read the raw file with the actual data
-	ifstream rawFile;
-	rawFile.open(rawFilename.c_str(), ios::binary);
+	std::ifstream rawFile;
+	rawFile.open(rawFilename.c_str(), std::ios::binary);
 	rawFile.sync_with_stdio(false);
 	if ( firstSecond > 0 ) {
-		rawFile.seekg(firstSecond * observation.getNrSamplesPerSecond() * nrSubbands * nrChannels, ios::beg);
+		rawFile.seekg(firstSecond * observation.getNrSamplesPerSecond() * nrSubbands * nrChannels, std::ios::beg);
 	}
 	data.resize(observation.getNrSeconds());
 
 	char * word = new char [4];
 	for ( unsigned int second = 0; second < observation.getNrSeconds(); second++ ) {
-		data.at(second) = new CLData< T >("second" + toStringValue< unsigned int >(second), true);
-		(data.at(second))->setDeviceReadOnly();
-		(data.at(second))->allocateHostData(observation.getNrChannels() * observation.getNrSamplesPerPaddedSecond());
+		data.at(second) = new std::vector< T >(observation.getNrChannels() * observation.getNrSamplesPerPaddedSecond());
 		for ( unsigned int sample = 0; sample < observation.getNrSamplesPerSecond(); sample++ ) {
 			for ( unsigned int subband = 0; subband < nrSubbands; subband++ ) {
 				for ( unsigned int channel = 0; channel < nrChannels; channel++ ) {
 					rawFile.read(word, 4);
-					bigEndianToLittleEndian(word);
-
-					(data.at(second))->setHostDataItem((static_cast< long long unsigned int >(second) * observation.getNrSamplesPerSecond()) + sample, *(reinterpret_cast< T * >(word)));
+          isa::utils::bigEndianToLittleEndian(word);
+          data.at(second)[(static_cast< long long unsigned int >(second) * observation.getNrSamplesPerSecond()) + sample] = *(reinterpret_cast< T * >(word));
 				}
 			}
 		}
