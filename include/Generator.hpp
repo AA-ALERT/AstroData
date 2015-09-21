8682309ea28975f26d85d552a3f9ce83a5a05ff8
Alessio Sclocco <alessio@sclocco.eu>
2015-09-21 17:16:33 +0200

    Added a single pulse generator.

--- include/Generator.hpp
+++ include/Generator.hpp
@@ -25,6 +25,7 @@
 namespace AstroData {
 
 template< typename T > void generatePulsar(const unsigned int period, const unsigned int width, const float DM, const AstroData::Observation & observation, std::vector< std::vector< T > * > & data, const bool random = false);
+template< typename T > void generateSinglePulse(const unsigned int width, const float DM, const AstroData::Observation & observation, std::vector< std::vector< T > * > & data, const bool random = false);
 
 // Implementations
 template< typename T > void generatePulsar(const unsigned int period, const unsigned int width, const float DM, const AstroData::Observation & observation, std::vector< std::vector< T > * > & data, const bool random) {
@@ -63,6 +64,54 @@ template< typename T > void generatePulsar(const unsigned int period, const unsi
         } else {
           data[second]->at((channel * observation.getNrSamplesPerPaddedSecond()) + internalSample) = 42;
         }
+      }
+    }
+  }
+}
+
+template< typename T > void generateSinglePulse(const unsigned int width, const float DM, const AstroData::Observation & observation, std::vector< std::vector< T > * > & data, const bool random) {
+  std::srand(std::time(0));
+  // Generate the  "noise"
+  for ( unsigned int second = 0; second < observation.getNrSeconds(); second++ ) {
+    data[second] = new std::vector< T >(observation.getNrChannels() * observation.getNrSamplesPerPaddedSecond());
+    if ( random ) {
+      for ( unsigned int channel = 0; channel < observation.getNrChannels(); channel++ ) {
+        for ( unsigned int sample = 0; sample < observation.getNrSamplesPerSecond(); sample++ ) {
+          data[second]->at((channel * observation.getNrSamplesPerPaddedSecond()) + sample) = std::rand() % 100;
+        }
+      }
+    } else {
+      std::fill(data[second]->begin(), data[second]->end(), 8);
+    }
+  }
+  // Generate the pulse
+  unsigned int second = 0;
+  unsigned int sample = 0;
+	float inverseHighFreq = 1.0f / (observation.getMaxFreq() * observation.getMaxFreq());
+  float kDM = 4148.808f * DM;
+
+  if ( random ) {
+    second = std::rand() % observation.getNrSeconds();
+    sample = std::rand() % (observation.getNrSamplesPerSecond() - width);
+  } else {
+    second = observation.getNrSeconds() / 2;
+    sample = observation.getNrSamplesPerSecond() / 2;
+  }
+
+  for ( unsigned int channel = 0; channel < observation.getNrChannels(); channel++ ) {
+    float inverseFreq = 1.0f / ((observation.getMinFreq() + (channel * observation.getChannelBandwidth())) * (observation.getMinFreq() + (channel * observation.getChannelBandwidth())));
+    float delta = kDM * (inverseFreq - inverseHighFreq);
+    unsigned int shift = static_cast< unsigned int >(delta * observation.getNrSamplesPerSecond());
+
+    for ( unsigned int i = 0; i < width; i++ ) {
+      if ( sample + i >= observation.getNrSamplesPerSecond() ) {
+      break;
+      }
+
+      if ( random ) {
+        data[second]->at((channel * observation.getNrSamplesPerPaddedSecond()) + (sample + shift)) = std::rand() % 500;
+      } else {
+        data[second]->at((channel * observation.getNrSamplesPerPaddedSecond()) + (sample + shift)) = 42;
       }
     }
   }
