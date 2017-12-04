cad69278045c3fd13d27c9ad696b0483714a8cd5
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2017-12-04 14:59:12 +0100

    Wrong use of Observation interface.

--- include/ReadData.hpp
+++ include/ReadData.hpp
@@ -80,11 +80,11 @@ template< typename T > void readSIGPROC(const Observation & observation, const u
   inputFile.seekg(bytesToSkip, std::ios::beg);
   for ( unsigned int batch = 0; batch < observation.getNrBatches(); batch++ ) {
     if ( inputBits >= 8 ) {
-      data.at(batch) = new std::vector< T >(observation.getNrChannels() * observation.getNrSamplesPerBatch(padding / sizeof(T)));
+      data.at(batch) = new std::vector< T >(observation.getNrChannels() * observation.getNrSamplesPerBatch(false, padding / sizeof(T)));
       for ( unsigned int sample = 0; sample < observation.getNrSamplesPerBatch(); sample++ ) {
         for ( unsigned int channel = observation.getNrChannels(); channel > 0; channel-- ) {
           inputFile.read(buffer, BUFFER_DIM);
-          data.at(batch)->at((static_cast< uint64_t >(channel - 1) * observation.getNrSamplesPerBatch(padding / sizeof(T))) + sample) = *(reinterpret_cast< T * >(buffer));
+          data.at(batch)->at((static_cast< uint64_t >(channel - 1) * observation.getNrSamplesPerBatch(false, padding / sizeof(T))) + sample) = *(reinterpret_cast< T * >(buffer));
         }
       }
     } else {
@@ -196,7 +196,7 @@ template< typename T > void readLOFAR(std::string headerFilename, std::string ra
 
   char * word = new char [4];
   for ( unsigned int batch = 0; batch < observation.getNrBatches(); batch++ ) {
-    data.at(batch) = new std::vector< T >(observation.getNrChannels() * observation.getNrSamplesPerBatch(padding / sizeof(T)));
+    data.at(batch) = new std::vector< T >(observation.getNrChannels() * observation.getNrSamplesPerBatch(false, padding / sizeof(T)));
     for ( unsigned int sample = 0; sample < observation.getNrSamplesPerBatch(); sample++ ) {
       for ( unsigned int subband = 0; subband < nrSubbands; subband++ ) {
         for ( unsigned int channel = 0; channel < nrChannels; channel++ ) {
@@ -204,7 +204,7 @@ template< typename T > void readLOFAR(std::string headerFilename, std::string ra
 
           rawFile.read(word, 4);
           isa::utils::bigEndianToLittleEndian(word);
-          data.at(batch)->at((globalChannel * observation.getNrSamplesPerBatch(padding / sizeof(T))) + sample) = *(reinterpret_cast< T * >(word));
+          data.at(batch)->at((globalChannel * observation.getNrSamplesPerBatch(false, padding / sizeof(T))) + sample) = *(reinterpret_cast< T * >(word));
         }
       }
     }
