3bec5761af94949b91f0b8b55b54383db36a9aaf
Alessio Sclocco <alessio@sclocco.eu>
2018-01-19 13:50:40 +0100

    Fixing a bug in which the number of subbands was not set when reading input from PSRDADA.

--- src/ReadData.cpp
+++ src/ReadData.cpp
@@ -82,7 +82,7 @@ void readPSRDADAHeader(Observation & observation, dada_hdu_t & ringBuffer) {
   ascii_header_get(header, "NCHAN", "%d", &uintValue);
   ascii_header_get(header, "MIN_FREQUENCY", "%f", &floatValue[0]);
   ascii_header_get(header, "CHANNEL_BANDWIDTH", "%f", &floatValue[1]);
-  observation.setFrequencyRange(1, uintValue, floatValue[0], floatValue[1]);
+  observation.setFrequencyRange(observation.getNrSubbands(), uintValue, floatValue[0], floatValue[1]);
   ascii_header_get(header, "TSAMP", "%f", &floatValue[0]);
   observation.setSamplingTime(floatValue[0]);
   if ( ipcbuf_mark_cleared(ringBuffer.header_block) < 0 ) {
